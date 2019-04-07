bf14e992a1fec89630cce1a88670ee7aa27a82e9
Shannon Foss <44273516+shafoss@users.noreply.github.com>
2019-04-07 08:20:02 -0500

    disable csv download feature

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_file
 from search import find_matches
 
 app = Flask(__name__)
@@ -15,12 +15,19 @@ def results():
         try:
             search_text = request.form['search-text']
             matches = find_matches(search_text=search_text)
-            return render_template('index.html', results="results.html", matches=matches)
+            return render_template('index.html', results="results.html", matches=matches, search_text=search_text)
         except Exception as e:
             print(e)
             return render_template('index.html', text="Something went wrong.")
     else:
         return redirect(url_for('index'))
+
+
+# @app.route("/download/<string:text>")
+# def download(text):
+#     filename = text + ".csv"
+#     results_to_csv(text)
+#     return send_file(filename, as_attachment=True)
 
 
 if __name__ == '__main__':
