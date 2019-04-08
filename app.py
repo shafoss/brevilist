e4b194b24d51197cbd43c4a9203ab055edeb6c53
Shannon Foss <44273516+shafoss@users.noreply.github.com>
2019-04-08 10:58:34 -0500

    Optimized search

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, send_file
-from search import find_matches
+from search import do_search
 
 app = Flask(__name__)
 
@@ -14,20 +14,13 @@ def results():
     if request.method == 'POST':
         try:
             search_text = request.form['search-text']
-            matches = find_matches(search_text=search_text)
+            matches = do_search(search_text=search_text)
             return render_template('index.html', results="results.html", matches=matches, search_text=search_text)
         except Exception as e:
             print(e)
             return render_template('index.html', text="Something went wrong.")
     else:
         return redirect(url_for('index'))
-
-
-# @app.route("/download/<string:text>")
-# def download(text):
-#     filename = text + ".csv"
-#     results_to_csv(text)
-#     return send_file(filename, as_attachment=True)
 
 
 if __name__ == '__main__':
--- search.py
+++ search.py
@@ -1,58 +1,27 @@
 import json
-# import pandas
+import re
 
 data = json.load(open("websters_dictionary.json"))
 
 
-def find_matches(search_text="", appear_in_order=True, single_word_matches=True):
-    if not search_text:
-        search_text = input("Enter letters: ")
-    letter_list = [x for x in search_text]
-
+def get_matches(search_pattern):
+    search_pattern = search_pattern
     matches = []
+    for key in data.keys():
+        if re.search(search_pattern, key.lower()):
+            matches.append(key)
+    return matches
 
-    for key in data:
-        lowercase_key = key.lower()
-        count = 0
-        if single_word_matches:
-            if " " in key:
-                continue
-        if not appear_in_order:
-            for letter in letter_list:
-                if letter in lowercase_key:
-                    count += 1
-                    break
-            if count == len(letter_list):
-                matches.append(key)
-        else:
-            place = 0
-            for i in range(0, len(letter_list), 1):
-                if letter_list[i] in lowercase_key:
-                    for k in range(place, len(lowercase_key), 1):
-                        if letter_list[i] == lowercase_key[k]:
-                            place = k + 1
-                            count += 1
-                            break
-                else:
-                    break
-            if count == len(letter_list):
-                matches.append(key)
+
+def do_search(search_text, appear_in_order=True):
+    letter_list = [x for x in search_text.lower()]
+
+    if appear_in_order:
+        search_pattern = '.*'.join(map(str, letter_list))
+    else:
+        search_pattern = '(?=.*' + ')(?=.*'.join(map(str, letter_list)) + ')'
+    matches = get_matches(search_pattern)
 
     matches.sort()
     results = {match.capitalize(): data[match] for match in matches}
     return results
-
-
-# def results_to_csv(text, matches):
-#     df = pandas.DataFrame(columns=["Match", "Definitions"])
-#     for match in matches:
-#         df = df.append({"Match": match, "Definitions": data[match]}, ignore_index=True)
-#     df.to_csv(filename)
-
-
-if __name__ == '__main__':
-    order = input("Match in order? (y,n) ").lower()
-    if order[0] == 'n':
-        find_matches(appear_in_order=False)
-    else:
-        find_matches()
