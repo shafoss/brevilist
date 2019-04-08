f794397c5dcb88e4bb22a7063b026a3fce3f611a
Shannon Foss <44273516+shafoss@users.noreply.github.com>
2019-04-08 13:06:35 -0500

    Added requirements and config file, CSRF protection, and ordered/unordered search feature.

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file
+from flask import Flask, render_template, request, redirect, url_for
+
+from config import Config
 from search import do_search
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+app.config.from_object(Config)
+csrf = CSRFProtect(app)
 
 
 @app.route('/')
@@ -14,8 +19,12 @@ def results():
     if request.method == 'POST':
         try:
             search_text = request.form['search-text']
-            matches = do_search(search_text=search_text)
-            return render_template('index.html', results="results.html", matches=matches, search_text=search_text)
+            in_order = request.form.get('in-order')
+            if search_text.isalpha():
+                matches = do_search(search_text, in_order)
+                return render_template('index.html', results="results.html", matches=matches, search_text=search_text)
+            else:
+                return render_template('index.html', help_text="Only alphabetic characters are allowed.")
         except Exception as e:
             print(e)
             return render_template('index.html', text="Something went wrong.")
